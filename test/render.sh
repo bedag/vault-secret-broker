56947050fc45103ec7569b37465e9032cfc123e6
Michael Gruener <michael.gruener@bedag.ch>
2019-10-23 12:51:10 +0200

    Basic vault test infrastructure code

---
+++ test/render.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Env variables (defaults in []):
+#   VSP_DEV_ID:       Used in the "dev-id" label to separate
+#                     multiple dev deployments int the same namespace ["dev"]
+#   VSP_DEV_REGISTRY: The registry hostname (including a trailing "/") to
+#                     prefix each image with [""]
+#   VSP_DEV_TOKEN:    The Vault root token ["123456790"]
+
+if [ -f "$(dirname $0)/env" ]; then
+  source "$(dirname $0)/env"
+fi
+
+echo "---"
+spruce merge vault-statefulset.yml
+echo "---"
+spruce merge vault-service.yml
